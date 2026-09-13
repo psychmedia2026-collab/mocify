c8c9af8e094fef8aaa287930f4d9d21d0d60400c
psychmedia2026-collab <psych.media2026@gmail.com>
2026-09-13 04:50:45 +0200

    Retry PNG logo with direct img tag

--- app/components.tsx
+++ app/components.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
 import Link from "next/link";
 import { featuredTrack, mobileNavigation, navigation, type Release } from "./data";
 
 export function Logo() {
   return <Link className="m-logo" href="/" aria-label="MOCIFY home">
-    <Image src="/mocify-bird.png?v=1" width={52} height={52} unoptimized alt="" />
+    <img src="/mocify-bird.png?v=2" width={52} height={52} alt="" aria-hidden="true" />
     <span>MOCIFY</span>
   </Link>;
 }
