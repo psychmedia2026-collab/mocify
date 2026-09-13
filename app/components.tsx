405b57c5a6b16e4aceac42a9188c53100d228d11
psychmedia2026-collab <psych.media2026@gmail.com>
2026-09-13 03:11:24 +0200

    Use approved MOCIFY wordmark in shared logo

--- app/components.tsx
+++ app/components.tsx
@@ -4,8 +4,14 @@ import { featuredTrack, mobileNavigation, navigation, type Release } from "./dat
 
 export function Logo() {
   return <Link className="m-logo" href="/" aria-label="MOCIFY home">
-    <Image src="/mocify-logo.svg" width={52} height={52} unoptimized alt="" />
-    <span>MOCIFY</span>
+    <Image
+      src="/mocify-logo-wordmark.webp"
+      width={360}
+      height={120}
+      unoptimized
+      alt="MOCIFY"
+      style={{ width: "190px", height: "auto", maxWidth: "42vw" }}
+    />
   </Link>;
 }
 
