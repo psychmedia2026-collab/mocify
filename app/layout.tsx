042072a7e8d6624e6a8a0fd7ffa8362f4f40fa06
psychmedia2026-collab <psych.media2026@gmail.com>
2026-09-17 23:42:47 +0200

    Remove legacy global hero overrides

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,6 @@ import "./i18n/language-switcher.css";
 import "./mobile-player-fix.css";
 import "./player-visualizer.css";
 import "./artist-dashboard-shell.css";
-import "./artist-hero-artwork.css";
-import "./artist-hero-photo-overrides.css";
 import { ListenerPlayerProvider } from "./listener-player";
 import { LanguageProvider } from "./i18n/language-provider";
 
