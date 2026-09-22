368f1f0ffb593186c509357db9c718e100b968bc
psychmedia2026-collab <psych.media2026@gmail.com>
2026-09-22 20:17:49 +0200

    Import Suspense for prerender boundaries

--- app/for-artists/manage-plan/page.tsx
+++ app/for-artists/manage-plan/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import {useEffect,useMemo,useState} from "react";
+import { Suspense, useEffect,useMemo,useState } from "react";
 import {useRouter,useSearchParams} from "next/navigation";
 import {ArtistPortalShell} from "../../artist-components";
 import {useLanguage} from "../../i18n/language-provider";
